90e353a80e0da641413a3eec57b459cf0564b183
Ezra <solorioje@gmail.com>
2023-04-25 19:42:40 -0600

    EDIT | role column added for User Model

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,9 +14,10 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('role')->default("User");
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->default(Hash::make('123456'));
             $table->rememberToken();
             $table->timestamps();
         });
