bd078570e72054b1e7bd79b8451e4a85d1ffac43
Ezra <solorioje@gmail.com>
2023-04-26 20:50:11 -0600

    ADD | Template of Animal Model

---
+++ database/migrations/2023_04_27_024427_create_animals_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('animals', function (Blueprint $table) {
+            $table->id();
+            $table->string('tag')->nullable();
+            $table->string('local_tag')->nullable();
+            $table->string('comments')->nullable();
+            $table->integer('person_id')->nullable()->unsigned();
+            $table->integer('animal_type_id')->unsigned();
+            $table->boolean('death')->default(false);
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('animals');
+    }
+};
