88220d18b856c4a3b058aa1e16e54437d8d42977
Ezra <solorioje@gmail.com>
2023-04-27 20:19:54 -0600

    ADD | Feeding of Feeding Model

---
+++ database/migrations/2023_04_28_021528_create_feedings_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('feedings', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('comments')->nullable();
+            $table->integer('feeding_type_id')->unsigned();
+            $table->integer('register_id')->unsigned();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('feedings');
+    }
+};
